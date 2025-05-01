033c98702b3aafcd77e6bfe7e2319c2beed106ef
Saiful Alam <saifulmr@hotmail.com>
2025-05-01 01:51:18 -0400

    Update ngx_http_auth_jwt_module.c fix for nginx 1.28

--- src/ngx_http_auth_jwt_module.c
+++ src/ngx_http_auth_jwt_module.c
@@ -902,7 +902,7 @@ static char *get_jwt(ngx_http_request_t *r, ngx_str_t jwt_location)
     jwt_location.len -= strlen(COOKIE_PREFIX);
 
 #ifndef NGX_LINKED_LIST_COOKIES
-    if (ngx_http_parse_multi_header_lines(&r->headers_in.cookies, &jwt_location, &jwtCookieVal) != NGX_DECLINED)
+    if (ngx_http_parse_multi_header_lines(r, r->headers_in.cookie, &jwt_location, &jwtCookieVal) != NGX_DECLINED)
     {
       has_cookie = true;
     }
